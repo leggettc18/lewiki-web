355423b459d9320b22e92ea83537a460acb97abf
Christopher Leggett <chris@leggett.dev>
2021-03-03 23:47:11 -0500

    Displays registration errors.

---
+++ src/utils/toErrorMap.ts
@@ -0,0 +1,10 @@
+import { FieldError } from "../generated/graphql";
+
+export const toErrorMap = (errors: FieldError[]) => {
+  const errMap: Record<string, string> = {};
+  errors.forEach(({ field, message }) => {
+    errMap[field] = message;
+  });
+
+  return errMap;
+};
