dc1bf4738ef8d6952805587f3c0d3b9b1177594c
Christopher Leggett <chris@leggett.dev>
2021-03-04 22:21:22 -0500

    Adds NavBar and conditionally renders login, register, and logout buttons.,

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import { Box, Flex, Link, Button } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+//interface NavBarProps {}
+
+export const NavBar = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link" color="black">
+          Logout
+        </Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
